364be7d395f798a130c3b8b335c88ae30d9a7d66
hdemon <zeitdiebe@gmail.com>
2016-12-01 21:28:13 +0900

    enable to specify languages through commandline
    diff --git a/package.json b/package.json
    index 0b63f15..c5ea897 100644
    --- a/package.json
    +++ b/package.json
    @@ -1,5 +1,6 @@
     {
       "dependencies": {
    +    "commander": "^2.9.0",
         "nightmare": "^2.8.1"
       },
       "name": "google-translation-cli",
    diff --git a/translate.js b/translate.js
    index baaf3a3..0184b51 100644
    --- a/translate.js
    +++ b/translate.js
    @@ -1,14 +1,23 @@
     #!/usr/bin/env node
     var Nightmare = require('nightmare');
    +var program = require('commander');
     var nightmare = Nightmare({ show: true });
    
    +// program
    +//   .version('0.0.1')
    +//   .option('-f, --from', 'a language you want to translate from')
    +//   .option('-t, --to', 'a language you want to translate to')
    +//   .parse(process.argv);
    +//
    +// console.log(program.from);
    +// console.log(process.argv[2]);
     nightmare
       .goto('https://translate.google.com/?hl=ja')
    -  .wait('#gt-sl-sugg > div[value="' + from + '"]')
    -  .wait('#gt-tl-sugg > div[value="' + to + '"]')
    -  .click('#gt-sl-sugg > div[value="' + from + '"]')
    -  .click('#gt-tl-sugg > div[value="' + to + '"]')
    -  .insert('#source', text)
    +  .wait('#gt-sl-sugg > div[value="' + program.from + '"]')
    +  .wait('#gt-tl-sugg > div[value="' + program.to + '"]')
    +  .click('#gt-sl-sugg > div[value="' + program.from + '"]')
    +  .click('#gt-tl-sugg > div[value="' + program.to + '"]')
    +  .insert('#source', process.argv[2])
       .click('#gt-submit')
       .wait('#result_box > span')
       .evaluate(function () {

--- translate.js
+++ translate.js
@@ -1,23 +1,37 @@
 #!/usr/bin/env node
 var Nightmare = require('nightmare');
+var program = require('commander');
 var nightmare = Nightmare({ show: true });
 
-nightmare
-  .goto('https://translate.google.com/?hl=ja')
-  .wait('#gt-sl-sugg > div[value="ja"]')
-  .wait('#gt-tl-sugg > div[value="en"]')
-  .click('#gt-sl-sugg > div[value="ja"]')
-  .click('#gt-tl-sugg > div[value="en"]')
-  .insert('#source', process.argv[2])
-  .click('#gt-submit')
-  .wait('#result_box > span')
-  .evaluate(function () {
-    return document.querySelector('#result_box').innerHTML.replace(/<\/*span>/g, '');
-  })
-  // .end()
-  .then(function (result) {
-    console.log(result)
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error);
+program
+  .version('0.0.1')
+  .arguments('<text>')
+  .option('-f, --from [language]', 'a language you want to translate from')
+  .option('-t, --to [language]', 'a language you want to translate to')
+  .action(function(text) {
+    console.log(text);
+    nightmare
+      .goto('https://translate.google.com/?hl=ja')
+      .wait('#gt-sl-sugg > div[value="ja"]')
+      .wait('#gt-tl-sugg > div[value="en"]')
+      .click('#gt-sl-sugg > div[value="' + program.from + '"]')
+      .click('#gt-tl-sugg > div[value="' + program.to + '"]')
+      .insert('#source', text)
+      .click('#gt-submit')
+      .wait('#result_box > span')
+      .evaluate(function () {
+        return document.querySelector('#result_box').innerHTML.replace(/<\/*span>/g, '');
+      })
+      // .end()
+      .then(function (result) {
+        console.log(result)
+      })
+      .catch(function (error) {
+        console.error('Search failed:', error);
+      });
   });
+
+program.parse(process.argv);
+
+// console.log(program.from);
+// console.log(process.argv[2]);
